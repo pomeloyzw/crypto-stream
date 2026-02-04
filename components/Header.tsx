008617af76f0da11cf0917c81019a827bbebc0c0
Pamela <akipyzw@gmail.com>
2026-02-05 01:11:35 +0800

    homepage with dummy data

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,28 +9,28 @@ const Header = () => {
 
 	const pathname = usePathname();
 
-  return (
-    <header>
-        <div className="main-container inner">
-            <Link href="/">
-                <Image src="/logo.svg" alt="CryptoPulse logo" width={132} height={40} />
-            </Link>
+	return (
+		<header>
+			<div className="main-container inner">
+				<Link href="/">
+					<Image src="/logo.svg" alt="CryptoPulse logo" width={132} height={40} />
+				</Link>
 
-            <nav>
-                <Link href="/" className={cn("nav-link", {
-									"is-active": pathname === "/",
-									"is-home": true
-								})}>Home</Link>
+				<nav>
+					<Link href="/" className={cn("nav-link", {
+						"is-active": pathname === "/",
+						"is-home": true
+					})}>Home</Link>
 
-                <p>Search Modal</p>
+					<p>Search Modal</p>
 
-                <Link href="/coins" className={cn("nav-link", {
-									"is-active": pathname === "/coins"
-								})}>All Coins</Link>
-            </nav>
-        </div>
-    </header>
-  )
+					<Link href="/coins" className={cn("nav-link", {
+						"is-active": pathname === "/coins"
+					})}>All Coins</Link>
+				</nav>
+			</div>
+		</header>
+	)
 }
 
 export default Header
